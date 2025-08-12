f07091d9103358cfb7a02d23cb31de31e164abf6
SARAH KHAN <you@example.com>
2025-08-12 06:01:16 +0530

    Fix: added home route for index.html

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ if __name__ == '__main__':
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    
+    from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
    
